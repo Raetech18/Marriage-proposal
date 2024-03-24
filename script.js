15c336b6dde439bd9f23cb12dcc470fa367d9b27
Raetech18 <164785408+Raetech18@users.noreply.github.com>
2024-03-24 19:07:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function respond(response) {
+    if (response === 'yes') {
+        alert("That's wonderful! I love you so much.");
+    } else if (response === 'no') {
+        alert("I understand. Thank you for your honesty.");
+    }
+}
